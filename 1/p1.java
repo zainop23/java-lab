73892e5c1b07254b85722ab5ab0e584cdc6775ea
zainop23 <anwerbasha@Zains-Mac.local>
2025-12-15 12:05:01 +0530

    1,2,3,8

---
+++ 1/p1.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class p1{
+    public static void main(String[] args) {
+        //Roots of Quadratic Equation
+        // x^2-3x+2
+        double a=1,b=-3,c=2;
+        double d = b*b - 4*a*c;
+        System.out.println("Roots : " + (-b + Math.sqrt(d)/2) + ", " + (-b - Math.sqrt(d)/2));
+        
+        //Multiply two arrays
+        int[] x={1,2,3}, y={2,3,4}, z=new int[3];
+        for(int i=0; i<3; i++){
+            z[i]=x[i]*y[i];
+        }
+        System.out.println("Product : " + Arrays.toString(z));
+
+        //Bubble Sort
+        int[] arr1 = {6,3,9,4,1};
+        int n = arr1.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr1[j] > arr1[j+1]){
+                    int temp = arr1[j];
+                    arr1[j]=arr1[j+1];
+                    arr1[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted Ascending Order : "+ Arrays.toString(arr1));
+
+        int[] arr2 = {6,3,9,4,1};
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(arr2[j] < arr2[j+1]){
+                    int temp = arr2[j];
+                    arr2[j]=arr2[j+1];
+                    arr2[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("Sorted Descending Order : "+ Arrays.toString(arr2));
+    }
+}
---
+++ 8/P8.java
@@ -0,0 +1,44 @@
+class P8{
+    int[] buf = new int[5];
+    int in=0,out=0,count=0;
+
+    synchronized void put(int n)throws InterruptedException{
+        while(buf.length==count) wait();
+        in = (in+1)%buf.length;
+        buf[in]=n;
+        count++;
+        System.out.println("Produced: "+n);
+        notifyAll();
+    }
+
+    synchronized int get()throws InterruptedException{
+        while(count==0) wait();
+        int item = buf[out];
+        out = (out+1)%buf.length;
+        count--;
+        System.out.println("Consumed: "+item);
+        notifyAll();
+        return item;
+    }
+    public static void main(String[] args) {
+        P8 pc = new P8();
+        new Thread(()->{
+            try {
+                for(int i=1; i<=10; i++){
+                    pc.put(i);
+                    Thread.sleep(500);
+                }
+            } catch (InterruptedException ignored) {}
+        }).start();
+
+        new Thread(()->{
+            try {
+                while(true){
+                    pc.get();
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException ignored) {}
+
+        }).start();
+    }
+}
